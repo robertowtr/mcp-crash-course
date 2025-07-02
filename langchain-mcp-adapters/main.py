187f7946b70fd49b0adad2ccc7a6bb1167d63cbf
Roberto Walter <roberto.walter@circle.co>
2025-07-02 11:58:11 -0300

    Add ChatOpenAI integration and StdioServerParameters setup in main.py; update pyproject.toml to include isort dependency; modify uv.lock to reflect new isort package.

--- langchain-mcp-adapters/main.py
+++ langchain-mcp-adapters/main.py
@@ -2,8 +2,27 @@ import asyncio
 from dotenv import load_dotenv
 import os
 
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY")) 
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/robertowalter/Documents/code/mcp-crash-course/langchain-mcp-adapters/servers/math_server.py"],
+)
+
+
+
+
+
+
 
 async def main():
     print("Hello from mcp-crash-course!")
