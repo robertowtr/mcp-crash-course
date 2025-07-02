432608e22d6daa06e22223e608c94014071dc75a
Roberto Walter <roberto.walter@circle.co>
2025-07-02 13:39:13 -0300

    Implement main function to initialize session and invoke agent with a sample math query; replace placeholder greeting with functional code.

--- langchain-mcp-adapters/main.py
+++ langchain-mcp-adapters/main.py
@@ -8,6 +8,7 @@ from mcp.client.stdio import stdio_client
 from langchain_openai import ChatOpenAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 
 
 load_dotenv()
@@ -19,13 +20,21 @@ stdio_server_params = StdioServerParameters(
 )
 
 
-
-
-
-
-
 async def main():
-    print("Hello from mcp-crash-course!")
+    # This communicate the Client with the Server
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session)
+            #print(tools)
+
+            agent = create_react_agent(llm, tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
+
+
 
 
 if __name__ == "__main__":
